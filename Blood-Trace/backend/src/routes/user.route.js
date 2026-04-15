1a7cd97f86578f9b17e078dfaa1f1c0ad993dd08
Umer Mujahid <umermujahid4738@gmail.com>
2026-04-15 13:40:31 +0500

    Removed Backend Folder

--- Blood-Trace/backend/src/routes/user.route.js
+++
@@ -1,9 +0,0 @@
-const { registerUser, loginUser } = require("../controllers/user.controller");
-const Router = require("express");
-const router = new Router();
-
-router.route('/register').post(registerUser);
-
-router.route('/login').post(loginUser);
-
-module.exports = router;
